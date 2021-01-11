9b510cc224b44b5c9cef21419d32efd0fef8541c
BuildTools <unconfigured@null.spigotmc.org>
2021-01-11 19:45:56 +0100

    test: new build script

--- hometracker/src/main/java/fr/owle/hometracker/listener/DefaultListenerRegisterer.java
+++ hometracker/src/main/java/fr/owle/hometracker/listener/DefaultListenerRegisterer.java
@@ -2,6 +2,8 @@ package fr.owle.hometracker.listener;
 
 import fr.owle.hometracker.HTAPI;
 import fr.owle.hometracker.events.EventManager;
+import fr.owle.hometracker.pages.Page;
+import fr.owle.hometracker.pages.PageManager;
 
 /**
  * DefaultListenerRegister register basic {@link fr.owle.hometracker.utils.Listener} for the {@link HTAPI}.
@@ -9,15 +11,28 @@ import fr.owle.hometracker.events.EventManager;
  */
 public class DefaultListenerRegisterer {
 
+    private EventManager eventManager;
+
+    public DefaultListenerRegisterer(EventManager eventManager) {
+        this.eventManager = eventManager;
+    }
+
+    public DefaultListenerRegisterer() {
+        this(HTAPI.getEvent().getEventManager());
+    }
+
     /**
      * Registers all basic {@link fr.owle.hometracker.utils.Listener}.
      * @param htapi The main module.
      * @see HTAPIPageRequestListener
      */
     public void register(HTAPI htapi) {
-        final EventManager eventManager = HTAPI.getEvent().getEventManager();
         final HTAPIPageRequestListener pageRequestListener = new HTAPIPageRequestListener();
+
         eventManager.registerEventListener(htapi, pageRequestListener);
     }
 
+    public EventManager getEventManager() {
+        return eventManager;
+    }
 }
--- hometracker/src/test/java/fr/owle/hometracker/listener/DefaultListenerRegisterTest.java
+++ hometracker/src/test/java/fr/owle/hometracker/listener/DefaultListenerRegisterTest.java
@@ -5,17 +5,27 @@ import fr.owle.hometracker.events.EventManager;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class DefaultListenerRegisterTest {
 
     @Test
     public final void registerTest() {
-        final DefaultListenerRegisterer defaultListenerRegisterer = new DefaultListenerRegisterer();
-        defaultListenerRegisterer.register(mock(HTAPI.class));
-        final EventManager eventManager = HTAPI.getEvent().getEventManager();
-        assertEquals(1, eventManager.getAllListener().size());
+        final EventManager eventManager = mock(EventManager.class);
+        final DefaultListenerRegisterer defaultListenerRegisterer = new DefaultListenerRegisterer(eventManager);
+        final HTAPI htapi = mock(HTAPI.class);
+        defaultListenerRegisterer.register(htapi);
+
+        verify(eventManager, times(1)).registerEventListener(eq(htapi), any(HTAPIPageRequestListener.class));
     }
+
+    @Test
+    public final void defaultEventManagerTest() {
+        final DefaultListenerRegisterer defaultListenerRegisterer = new DefaultListenerRegisterer();
+        assertEquals(HTAPI.getEvent().getEventManager(), defaultListenerRegisterer.getEventManager());
+    }
+
+
 
 
 }
