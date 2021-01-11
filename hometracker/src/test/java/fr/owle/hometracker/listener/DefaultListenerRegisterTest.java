d9e30023c5803ee7e2b1b1bb9b4c3ca1b09f75b9
BuildTools <unconfigured@null.spigotmc.org>
2021-01-11 19:37:28 +0100

    test: new build script

--- hometracker/src/test/java/fr/owle/hometracker/listener/DefaultListenerRegisterTest.java
+++ hometracker/src/test/java/fr/owle/hometracker/listener/DefaultListenerRegisterTest.java
@@ -5,13 +5,14 @@ import fr.owle.hometracker.events.EventManager;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class DefaultListenerRegisterTest {
 
     @Test
     public final void registerTest() {
         final DefaultListenerRegisterer defaultListenerRegisterer = new DefaultListenerRegisterer();
-        defaultListenerRegisterer.register((HTAPI) HTAPI.getHTAPI());
+        defaultListenerRegisterer.register(mock(HTAPI.class));
         final EventManager eventManager = HTAPI.getEvent().getEventManager();
         assertEquals(1, eventManager.getAllListener().size());
     }
